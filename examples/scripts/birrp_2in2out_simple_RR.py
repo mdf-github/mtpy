ccbe8a02069537342a55df7898217dbc43c56db5
mdf-github <mikaela.irene@gmail.com>
2015-07-10 11:00:30 +0800

    fixed bug in birrp_2in2out_simple_RR.py; starttime and endttime explicitly stated, _Nin2out_ instead of _2in2out_ called

--- examples/scripts/birrp_2in2out_simple_RR.py
+++ examples/scripts/birrp_2in2out_simple_RR.py
@@ -102,7 +102,7 @@ def main():
         ## MTbp.runbirrp2in2out_simple(birrp_exe, stationname, ts_dir,coherence_th, 
                                     ## rr_stationname, None, starttime, endtime)
         MTbp.runbirrp_Nin2out_simple(birrp_exe, stationname, ts_dir,coherence_th, 
-                                    rr_stationname, None, starttime, endtime)
+                                    rr_stationname, starttime=starttime, endtime=endtime)
     # except:
     #     print '\n\tERROR - Could not process input data using BIRRP\n'
     #     return
